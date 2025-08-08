95f75c0304e9a13a1ab4f9089acef573e04ba427
Debanjan Raha <rahadebanjan2005@gmail.com>
2025-08-08 23:45:27 +0530

    discounted price.py

--- discounted price.py
+++ discounted price.py
@@ -1,7 +1,8 @@
 p=int(input("Enter your total price:"))
 if p>=20000 and p<30000:
-    print("Discounted price=",0.05*p)
+    print("Discounted price=",0.95*p)
 elif p>=30000 and p<50000:
-    print("Discounted price=",0.10*p)
+    print("Discounted price=",0.90*p)
 else:
-    print("Discounted price=",0.15*p)
+    print("Discounted price=",0.85*p)
+
